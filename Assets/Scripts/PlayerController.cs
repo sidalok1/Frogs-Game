d83ba43ff056d8c15321ed792bf2d78ad7047f90
eciujeye <claireoh2004@gmail.com>
2023-10-26 17:00:17 -0700

    testing push

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hi sid
     }
 
     // Update is called once per frame
