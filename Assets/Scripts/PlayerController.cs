6098a2794b0a0c21d90d2947be1c79bd1658fed2
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2023-10-31 01:43:05 -0700

    Added basics of 2D side scroller movement
    +Imported free asset pack from Unity Asset Store for possibly temporary visual assets
    +Added player model and environment features such as background and terrain
    +Added left-right player movement
    
    To Do:
    ~Implement Jumping
    ~Other potentially movement related gameplay elements such as frog tongue
    ~Fix animation sprites for player model (currently frozen when idle and playing idle animation when moving)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //hi sid
+    
+    [SerializeField] private float speed;
+    //Speed is the acceleration factor to be applied to the input in order to calculate the impulse added
+
+    [SerializeField] private float spdLimit;
+    //If the current x - velocity exceeds the speed limit, do not add force. Only works with drag and friction.
+    private Rigidbody2D rb;
+    private float movX;
+    private float movY;
+    private Vector2 movForce;
+    private bool facingLeft = false;
+    //If player puts left input while facing right and vice versa, the sprite flips directions
+    void Start() {
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void FixedUpdate() {
+        //Need to add jump and ensure that jumping is not contingent on being under speed limit
+        if (Mathf.Abs(rb.velocity.x) < spdLimit) {
+            movForce = new Vector2(movX, movY);
+            rb.AddForce(movForce * speed, ForceMode2D.Impulse);
+        }
+        
+
+    }
+
+    void OnMove(InputValue mov) {
+        Vector2 v = mov.Get<Vector2>();
+
+        if (v.x > 0.0f && facingLeft) {
+            transform.Rotate(0.0f, -180.0f, 0.0f, Space.Self);
+            Debug.Log(1);
+            facingLeft = false;
+        } else if (v.x < 0.0f && !facingLeft) {
+            Debug.Log(2);
+            transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+            facingLeft = true;
+        }
+        
+        movX = v.x;
         
     }
 }
