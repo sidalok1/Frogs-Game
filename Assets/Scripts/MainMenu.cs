490361c05a20d2e2d1d7e3148176969a3d1649ca
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2023-10-31 02:45:15 -0700

    Added keys to control screen
    +Added sprites for various keyboard controls from https://dreammix.itch.io/keyboard-keys-for-ui
    +Added horizontal movement to control screen
    +Fixed control screen being set active on starting the game
    To Do:
    ~Make menu more visually striking with artwork possibly
    ~Connect with main branch

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject controlScreen;
+    private void Start() {
+        controlScreen.SetActive(false);
+    }
+    
     public void OnPlayButton () { // goes into game
         SceneManager.LoadScene(1); // loads second scene
     }
