e76fdd3c35fdf2564019ac2b6ac624f1f4860866
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2023-11-10 14:04:18 -0800

    Added Wall Jumping
    +New check to see if player is on wall
    +If player is on wall their velocity is set to constant value
    -Jumping no longer uses addForce, as this caused issues with player being able to gain absurd y velocity through wall jumping
    ~Rearranged code to become more readable
    ~Player colliders simplified to just a capsule collider. Triggers no longer needed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float spdLimit;
     //If the current x - velocity exceeds the speed limit, do not add force. Only works with drag and friction.
     [SerializeField] private float jmpVal;
+    private float modjmp;
     private Rigidbody2D rb;
     private float movX;
     private float movY;
@@ -19,11 +20,14 @@ public class PlayerController : MonoBehaviour
     private bool facingLeft;
     //If player puts left input while facing right and vice versa, the sprite flips directions
     private bool canJump;
+    private bool wallJump;
+    private bool jumping;
     // private int[] iter = {0, 0};
     void Start() {
         rb = GetComponent<Rigidbody2D>(); 
         animator.SetBool("isMoving", false);
         facingLeft = false;
+        modjmp = jmpVal * 0.8f;
     }
 
     private void Update() {
@@ -34,13 +38,6 @@ public class PlayerController : MonoBehaviour
         }
         if (rb.velocity.y > 0.5f) {
             animator.SetBool("ascending", true);
-            /* iter[0] += 1;
-            if (iter[0] == 3) {
-                // animator.SetBool("ascending", true);
-                iter[0] = 0;
-            } */
-        } else {
-            // iter[0] = 0;
         }
         if (rb.velocity.y < -0.5f) {
             animator.SetBool("falling", true);
@@ -55,20 +52,48 @@ public class PlayerController : MonoBehaviour
             // iter[1] = 0;
         }
         if (canJump == true) {
-            
             animator.SetBool("ascending", false);
             animator.SetBool("falling", false);
         }
-        Debug.Log(rb.velocity.y);
+        //Debug.Log(rb.velocity.y);
     }
 
     private void FixedUpdate() {
-        //Need to add jump and ensure that jumping is not contingent on being under speed limit
         if (Mathf.Abs(rb.velocity.x) < spdLimit) {
-            movForce = new Vector2(movX, 0.0f);
-            rb.AddForce(movForce * speed, ForceMode2D.Impulse);
+           movForce = new Vector2(movX, 0.0f);
+           rb.AddForce(movForce * speed, ForceMode2D.Impulse);
         }
-        
+        if (jumping == true) {
+            jump();
+        }
+    }
+
+    void jump() {
+        if (canJump) {
+            //movForce = new Vector2(0.0f, jmpVal);
+            //rb.AddForce(movForce, ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, jmpVal);
+            canJump = false;
+            animator.SetBool("onGround", false); 
+            return;
+        } else if (wallJump) {
+            if (facingLeft) {
+                transform.Rotate(0.0f, -180.0f, 0.0f, Space.Self);
+                facingLeft = false;
+                //movForce = new Vector2(jmpVal, modjmp);
+                //rb.AddForce(movForce, ForceMode2D.Impulse);
+                rb.velocity = new Vector2(modjmp, jmpVal);
+            } else if (!facingLeft) {
+                transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+                facingLeft = true;
+                //movForce = new Vector2(-jmpVal, modjmp);
+                //rb.AddForce(movForce, ForceMode2D.Impulse);
+                rb.velocity = new Vector2(-modjmp, jmpVal);
+            }
+        }
+    }
+    void OnJump () {
+        jumping = !jumping;
     }
 
     void OnMove(InputValue mov) {
@@ -86,24 +111,26 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    void OnJump () {
-        if (canJump) {
-            movForce = new Vector2(0.0f, jmpVal);
-            rb.AddForce(movForce, ForceMode2D.Impulse);
-        } 
-    }
+    
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Ground")) {
             canJump = true;
             animator.SetBool("onGround", true);
         }
+        if (other.gameObject.CompareTag("Wall")) {
+            wallJump = true;
+        }
+        
     }
 
     private void OnCollisionExit2D(Collision2D other) {
         if (other.gameObject.CompareTag("Ground")) {
-            canJump = false;
-            animator.SetBool("onGround", false);
+            /* canJump = false;
+            animator.SetBool("onGround", false); */
+        }
+        if (other.gameObject.CompareTag("Wall")) {
+            wallJump = false;
         }
     }
 }
