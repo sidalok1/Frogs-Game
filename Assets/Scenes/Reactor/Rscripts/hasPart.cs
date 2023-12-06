1d28e9733548165f8ee4cdb9b50ced8ed0f933b4
eciujeye <claireoh2004@gmail.com>
2023-12-06 14:27:07 -0800

    Inventory updated assets

---
+++ Assets/Scenes/Reactor/Rscripts/hasPart.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hasPart : MonoBehaviour
+{
+    [Header("Button")]
+    [SerializeField] public GameObject button;
+    // Start is called before the first frame update
+
+    // public bool hasChild() {
+
+    //     if(transform.childCount > 0) {
+    //         button.SetActive(true);
+    //     } else {
+    //         button.SetActive(false);
+    //     }
+    // }
+
+}
