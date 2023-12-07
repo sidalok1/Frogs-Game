a5ffc42450107896cc983e80b92d3f938366b98d
eciujeye <claireoh2004@gmail.com>
2023-12-06 18:24:39 -0800

    finished reactor scenes

--- Assets/Scenes/Reactor/Rscripts/hasPart.cs
+++ Assets/Scenes/Reactor/Rscripts/hasPart.cs
@@ -9,13 +9,29 @@ public class hasPart : MonoBehaviour
     [SerializeField] public GameObject button;
     // Start is called before the first frame update
 
-    // public bool hasChild() {
+    [Header("banner")]
+    [SerializeField] public GameObject Banner;
 
-    //     if(transform.childCount > 0) {
-    //         button.SetActive(true);
-    //     } else {
-    //         button.SetActive(false);
-    //     }
-    // }
+    [Header("Fan Not Overlapped")]
+
+     [Header("Fan Overlapped")]
+    [SerializeField] public GameObject fan1;
+
+    private void Update() {
+        hasChild();
+    }
+
+    private void hasChild() {
+
+        if(transform.childCount > 0) {
+            button.SetActive(true);
+            Banner.SetActive(true);
+            fan.SetActive(false);
+            fan1.SetActive(true);
+        } else {
+            //button.SetActive(false);
+            //Banner.SetActive(true);
+        }
+    }
 
 }
